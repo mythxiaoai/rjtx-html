593c2690d85d31b92fcf14bfeb8eb3c927618340
Darlene-rong <z519564229@qq.com>
2019-02-22 08:55:46 +0800

    页面添加

---
+++ js/newdata.js
@@ -0,0 +1,78 @@
+[
+    {
+        "menus":[
+            {
+                "id": "0",
+                "name": "首页",
+                "url": "",
+                "icon": "iconfont icon-shouye1",
+                "pid": ""
+            },{
+                "id": "01",
+                "name": "首页",
+                "url": "newpage/home/home.html" ,
+                "icon": "iconfont icon-shouye",
+                "pid": "0" 
+            },
+            {
+                "id": "1",
+                "name": "统计图表",
+                "url": "",
+                "icon": "iconfont icon-zhexiantu",
+                "pid": ""
+            },{
+                "id": "10",
+                "name": "百度Echart",
+                "url": "",
+                "icon": "glyphicon glyphicon-magnet",
+                "pid": "1"
+            },
+            {
+                "id": "2",
+                "name": "表单",
+                "url": "",
+                "icon": "iconfont icon-bingzhuangtu",
+                "pid": "" 
+            },{
+                "id": "21",
+                "name": "基本表单",
+                "url": "newpage/forms/form_basic.html",
+                "icon": "glyphicon glyphicon-edit",
+                "pid": "2"
+            },{
+                "id": "22",
+                "name": "表单验证",
+                "url": "newpage/forms/form_verification.html",
+                "icon": "fa fa-cubes",
+                "pid": "2"
+            },{
+                "id": "22",
+                "name": "表单选择器",
+                "url": "newpage/forms/form_datetimepicker.html",
+                "icon": "fa fa-cubes",
+                "pid": "2"
+            },
+            {
+                "id": "3",
+                "name": "插件",
+                "url": "",
+                "icon": "iconfont icon-mianji",
+                "pid": "" 
+            },
+            {
+                "id": "4",
+                "name": "UI元素",
+                "url": "",
+                "icon": "iconfont icon-dengji",
+                "pid": "" 
+            },
+            {
+                "id": "5",
+                "name": "CSS动画",
+                "url": "",
+                "icon": "fa fa-magic",
+                "pid": "" 
+            }
+        ]
+    }
+]
